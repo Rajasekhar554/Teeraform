2919c43fb84b0566cd6c8e4d1f665ec0c1f640cc
Rajasekhar554 <67279366+Rajasekhar554@users.noreply.github.com>
2026-05-30 21:18:18 +0530

    Add files via upload

---
+++ DAY03/main.tf
@@ -0,0 +1,35 @@
+terraform {
+    required_providers {
+
+       aws = {
+          source = "hashicorp/aws"
+          version = "~>6.0"
+}
+
+}
+# backend configuration
+
+  backend "s3" {
+    bucket         = "rajasekhar-tf-state"
+    key            = "dev/terraform.tfstate"
+    region         = "ap-south-1"
+    use_lockfile  = "true"
+    encrypt        = true
+  }
+}
+
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+
+resource "aws_s3_bucket" "Main1234" {
+  bucket =  " rajasekhar-2026-May-30-bucket-for-terraformstatefile"
+    
+    tags = {
+      name = "raja-2026"
+      Env = "staging"
+    }
+
+}
