cda5f1c98f87ff1146c0fcb3667d6bf64109e67b
Rajasekhar554 <67279366+Rajasekhar554@users.noreply.github.com>
2026-05-30 21:31:07 +0530

    Update main.tf

--- DAY03/main.tf
+++ DAY03/main.tf
@@ -1,37 +1,38 @@
 terraform {
-    required_providers {
+  required_providers {
 
-       aws = {
-          source = "hashicorp/aws"
-          version = "~>6.0"
-}
-
-}
-# backend configuration
-
-  backend "s3" {
-    bucket         = "rajasekhar-tf-state"
-    key            = "dev/terraform.tfstate"
-    region         = "ap-south-1"
-    use_lockfile  = "true"
-    encrypt        = true
-  }
-}
-
-
-provider "aws" {
-  region = "ap-south-1"
-}
-
-
-resource "aws_s3_bucket" "Main1234" {
-  bucket =  " rajasekhar-2026-May-30-bucket-for-terraformstatefile"
-    
-    tags = {
-      name = "raja-2026"
-      Env = "staging"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>6.0"
     }
 
+  }
+  # backend configuration
+
+  backend "s3" {
+    bucket       = "rajasekhar-tf-state"
+    key          = "dev/terraform.tfstate"
+    region       = "ap-south-1"
+    use_lockfile = "true"
+    encrypt      = true
+  }
+
 }
+
+  provider "aws" {
+    region = "ap-south-1"
+  }
+
+
+  resource "aws_s3_bucket" "Main1234" {
+    bucket = "rajasekhar-2026-may-30-tfstate-12345"
+
+    tags = {
+      name = "raja-2026"
+      Env  = "staging"
+    }
+  }
+
+
 
 Note :: create s3 bucket through backend
